064cc6ba59c59319ffb22ce631acc44d1dc9c913
ryanskiefelipe <ryanskiefelipe@gmail.com>
2015-08-19 17:46:22 +0800

    asd

---
+++ application/controllers/test_page.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test_page extends CI_Controller {
+	public function index() {
+		$this->load->view('test_page/index');
+	}
+}
